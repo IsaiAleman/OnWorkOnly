2488ebb45216552ffccc682889edc4b5f9f40cbe
Hector Can <habacab@gmail.com>
2020-04-11 21:48:53 -0500

    Installed dependencie of Buefy

--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
+import axios from 'axios';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.min.css';
 import Router from 'vue-router';
 import Index from '../components/Index.vue';
 import Login from '../components/authentication/Login.vue';
 import Register from '../components/authentication/Register.vue';
 
 Vue.use(Router);
+Vue.use(axios);
+Vue.use(Buefy);
 
 export default new Router({
   mode: 'history', // use HTML5 history instead of hashes
